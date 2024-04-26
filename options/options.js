de8fbf25ed18817fe9ba6c21ab1a0fd325660bce
Crowdbotics <team@crowdbotics.com>
2024-04-26 21:51:12 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "solitary-river-47723",
+  url: "https://solitary-river-47723.botics.co",
+  api: "https://solitary-river-47723.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
